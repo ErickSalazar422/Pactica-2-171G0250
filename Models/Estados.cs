b99df6a908fbc8e0401d491149fafe8fc72424d2
ErickSalazar422 <71106495+ErickSalazar422@users.noreply.github.com>
2020-09-14 20:21:39 -0500

    first commit

---
+++ Models/Estados.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Actividad2_171G0250.Models
+{
+    public partial class Estados
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Abrev { get; set; }
+    }
+}
